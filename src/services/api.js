dd26e50a04271ea0c3dd4f29c21796ff81710296
Iurii Bilonog <iuriibilonog@gmail.com>
2021-11-07 20:08:54 +0200

    done all

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,18 @@
 import axios from "axios";
 
-export async function getDataServer(query, page) {
-  const response = await axios(
-    `https://pixabay.com/api/?q=${query}&page=${page}&key=23766907-8949d781ce5b5ece952eeda6b&image_type=photo&orientation=horizontal&safesearch=true&per_page=12`
+export function getDataServer(query, page) {
+  const searchParams = {
+    key: "23766907-8949d781ce5b5ece952eeda6b",
+    image_type: "photo",
+    orientation: "horizontal",
+    safesearch: true,
+    per_page: 12,
+  };
+
+  const response = axios.get(
+    `https://pixabay.com/api/?q=${query}&page=${page}`,
+    { params: searchParams }
   );
 
-  if (response.status >= 200 && response.status < 300) {
-    return response.data;
-  }
-  throw new Error(
-    console.log(
-      "Sorry, there are no images matching your search query. Please try again."
-    )
-  );
+  return response.then(({ data }) => data);
 }
